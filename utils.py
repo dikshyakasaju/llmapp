b02ba08effbe50e499b9db613c9a6d514dd5436b
‘Dikshya <dikshyakasaju@gmail.com>
2024-02-12 13:24:45 +0530

    change in model and codebase

--- utils.py
+++ utils.py
@@ -1,112 +1,122 @@
 #Import deps
-from langchain.vectorstores import Chroma, Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain import OpenAI
-import tiktoken
-from langchain.chat_models import ChatOpenAI 
 import os
-from langchain.agents import Tool
+import yaml
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
+from langchain.agents import create_react_agent
+from langchain_experimental.pal_chain import PALChain
 from langchain.agents import load_tools
-from sqlalchemy import MetaData
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Table, Date, Float
-import pandas as pd
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.agents import initialize_agent
-import streamlit as st
-from dotenv import load_dotenv
-from langchain.chains import RetrievalQA
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain_core.runnables import (
+    RunnableParallel,
+    RunnablePassthrough
+)
+from langchain.schema.output_parser import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
-# # Load variables from .env file
-# load_dotenv()
+# Access the API key
+api_key = ''
 
-# # Access the API key
-# openai_api_key = os.getenv('OPENAI_API_KEY')
+# Loading the prompts:
+with open("prompts.yaml", "r") as yaml_file:
+        data = yaml.safe_load(yaml_file)
 
-OPENAI_API_KEY = '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'
-# Intialize the LLM text-davinci-003 model
-@st.cache_resource
-def load_llm_model(temperature = 0.5):
-    llm = OpenAI(
-        openai_api_key = OPENAI_API_KEY,
-        model_name = 'text-davinci-003',
-        temperature = temperature
+
+# LLM Model
+def load_llm_model(temperature: float = 0.0):
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-pro",
+        temperature=temperature,
+        google_api_key=api_key
     )
     return llm
 
+# Python agent
+def load_python_agent(model):
 
-
-@st.cache_resource
-def load_gptturbo_model(temperature = 0.5):
-    llm = OpenAI(
-        openai_api_key = OPENAI_API_KEY,
-        model_name = 'gpt-3.5-turbo',
-        temperature = temperature
-    )
-    return llm
-
-
-@st.cache_resource
-def openai_embedding():
-    embeddings = OpenAIEmbeddings(
-        model='text-embedding-ada-002',
-        openai_api_key=OPENAI_API_KEY
-    )
-    return embeddings
-
-
-@st.cache_data
-def tiktoken_len(text):
-    tokenizer = tiktoken.get_encoding('cl100k_base')
-    tokens = tokenizer.encode(
-        text,
-        disallowed_special=()
-    )
-    return len(tokens)
+    python_tool_instructions = data['python_tool_instructions']
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=python_tool_instructions)
+    # Creating Python Tool
+    python_tool = [PythonREPLTool()]
+    # Creating a Python agent
+    python_agent = create_react_agent(llm=model, tools=python_tool, prompt=prompt)
+    #Agent Executor
+    agent_executor = AgentExecutor(agent=python_agent, tools=python_tool, verbose=True)
+    return agent_executor
 
 
 
-@st.cache_data
-def split_text(chunk_size, chunk_overlap):
-    text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size = chunk_size,
-                chunk_overlap = chunk_overlap,
-                length_function=len,
-                #separators=["\n\n", "\n", " ", ""]
-    )
-    return text_splitter
+def load_pal_agent(model):
+
+    pal_chain = PALChain.from_math_prompt(llm=model, verbose=True)
+    return pal_chain
 
 
-@st.cache_data
-def create_memory():
-    conversational_memory = ConversationBufferWindowMemory(
-        memory_key='chat_history',
-        k=5,
-        return_messages=True
-    )
-    return conversational_memory
 
-@st.cache_data
-def create_retrievalqa(model, retriever):
-    chromaqa = RetrievalQA.from_chain_type(
+
+def load_serp_agent(model):
+
+    os.environ['SERPAPI_API_KEY'] = ''
+    serp_tool = load_tools(tool_names=["serpapi"])
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    serp_tool_instructions = data['serp_tool_instructions']
+    prompt = base_prompt.partial(instructions=serp_tool_instructions)
+    serp_agent = create_react_agent(
         llm=model,
-        chain_type="stuff",
-        retriever=retriever.as_retriever() 
+        tools=serp_tool,
+        prompt=prompt)
+    serp_agent = AgentExecutor(agent=serp_agent, tools=serp_tool, verbose=True)
+    return serp_agent
+
+
+
+def load_vector_agent(model):
+    # Pinecone Initialization
+    YOUR_API_KEY = ''
+    YOUR_ENV = "us-west4-gcp-free"
+    text_field = "text"
+    index_name = 'wwts'
+    # Initialize the Pinecone client.
+    pinecone.init(
+        api_key=YOUR_API_KEY,
+        environment=YOUR_ENV
     )
-    return chromaqa
+    index = pinecone.Index(index_name)
+    # Instantiate the Google embedding model
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
+    # Create Pinecone VectorDB object using with Langchain
+    vectorstore = Pinecone(index=index, embedding=embeddings.embed_query, text_key=text_field)
+    retriever = vectorstore.as_retriever()
+    vector_prompt_template = data['vector_prompt_template']
+    prompt = ChatPromptTemplate.from_template(vector_prompt_template)
+    # Build RAG
+    rag = RunnableParallel(
+        {"context": retriever, "question": RunnablePassthrough()}
+    )
+    # Output parser to convert output to string
+    output_parser = StrOutputParser()
+    # Building RAG chain
+    rag_agent = rag | prompt | model | output_parser
 
+    return rag_agent
 
-
-def card():
-    return """
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div>
-        </div> 
-    """
+#
+#
+# def card():
+#     return """
+#         <div class="card" style="width: 18rem;">
+#             <div class="card-body">
+#                 <h5 class="card-title">Card title</h5>
+#                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+#                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+#                 <a href="#" class="card-link">Card link</a>
+#                 <a href="#" class="card-link">Another link</a>
+#             </div>
+#         </div>
+#     """
